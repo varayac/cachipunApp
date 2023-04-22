5305476806a46c2a35427ddff522f47a4d717d18
= <=>
2023-04-22 12:32:38 -0400

    ver 1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,27 @@
 // OBTENER
+let ingresaJugada
 let cantidadJugadas = parseInt(prompt("Cuantas rondas desea jugar"));
 // let seleccionUsuario = parseInt(prompt("ingrese su opcion"));
 
 for (let i = 0; i < cantidadJugadas; i++) {
   do {
-    let ingresaJugada = prompt(
-      "ingrese su jugada [Piedra], [Papel], [Tijeras]"
-    ).toLowerCase();
-  } while (
-    ingresaJugada == "Piedra" ||
-    ingresaJugada == "Papel" ||
-    ingresaJugada == "Tijeras"
-  );
+    ingresaJugada = prompt("ingrese su jugada [Piedra], [Papel], [Tijeras]").toLowerCase();
+  } while (ingresaJugada != "piedra" && ingresaJugada != "papel" && ingresaJugada != "tijeras");
+  play(ingresaJugada)
+}
+
+function play(jugadaUsuario){
+    let playpc = Math.floor(Math.random()*3)
+    switch (playpc) {
+        case 0:
+            playpc = "piedra"
+            break
+        case 1:
+            playpc = "papel"
+            break
+        case 2:
+            playpc = "tijeras"
+            break
+    }
+    console.log("usuario : " + jugadaUsuario + " pc: " + playpc)
 }
